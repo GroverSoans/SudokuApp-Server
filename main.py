1a96e19b007d6f9451523da783b27b809e6d533e
GroverSoans <groversoans@Grovers-Air.zyxel.com>
2024-08-07 18:07:15 -0500

    Flask app connected with React frontend

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, origins='*')  # This will enable CORS for all routes
 
-@app.route('/')
-def home():
-    return jsonify(message="Hello, world!")
+@app.route("/api/users", methods = ['GET'])
+def users():
+    return jsonify(
+                   {
+                       "users": [
+                           'grover',
+                           'ben',
+                           'enigma'
+                       ]
+                       
+                   }
+                )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
